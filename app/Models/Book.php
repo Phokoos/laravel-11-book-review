30484f244f688ac368654ab6b581ce4825672fee
Микола <hulkovskij@gmail.com>
2024-06-15 14:46:15 +0300

    Finished 65

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -62,4 +62,38 @@ class Book extends Model
             $query->whereBetween('created_at', [$from, $to]);
         }
     }
+
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopePopularLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn (Book $book) => cache()->forget('book:' . $book->id));
+        static::deleted(fn (Book $book) => cache()->forget('book:' . $book->id));
+    }
 }
